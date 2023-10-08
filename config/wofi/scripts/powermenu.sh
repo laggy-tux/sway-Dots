302e1f50bff0a28d38feb1ef1c3b17d49959f962
Joy Yahshua <85402808+laggy-tux@users.noreply.github.com>
2023-10-08 20:36:26 +0530

    Add files via upload

---
+++ config/wofi/scripts/powermenu.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+entries="⇠ Logout\n⏾ Suspend\n⭮ Reboot\n⏻ Shutdown"
+
+selected=$(echo -e $entries|wofi --width 250 --height 210 --xoffset=1660 --yoffset=0 --prompt=leaving? --dmenu --cache-file /dev/null | awk '{print tolower($2)}')
+
+case $selected in
+  logout)
+    swaymsg exit;;
+  suspend)
+    exec systemctl suspend;;
+  reboot)
+    exec systemctl reboot;;
+  shutdown)
+    exec systemctl poweroff -i;;
+esac
